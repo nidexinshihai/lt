5d16183ec7efc861e8a7f74e17ba57c7bf7d401e
xzh <xzh>
2020-05-10 22:26:14 +0800

    init repo

---
+++ README.md
@@ -0,0 +1,4 @@
+#工具Intellj IDEA
+
+#资料
+https://spring.io/guides/gs/serving-web-content/
---
+++ src/main/java/com/lt/xzh/XzhApplication.java
@@ -0,0 +1,13 @@
+package com.lt.xzh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XzhApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XzhApplication.class, args);
+    }
+
+}
